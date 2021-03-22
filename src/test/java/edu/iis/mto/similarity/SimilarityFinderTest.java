e86d634ed9afa5e79282f5c488286c27935d86b8
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-03-22 18:27:38 +0100

    testEmptyArrays added

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,20 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testEmptyArrays() {
+        int[] array = {};
+        int[] array2 = {};
+        SequenceSearcher sequenceSearcher = (elem, sequence) -> SearchResult.builder().build();
+        SimilarityFinder finder = new SimilarityFinder(sequenceSearcher);
+        assertEquals(1,finder.calculateJackardSimilarity(array, array2));
     }
 
 }
