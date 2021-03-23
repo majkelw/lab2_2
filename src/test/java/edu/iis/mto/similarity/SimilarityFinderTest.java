01552607610dcd5067ece78ad06a2b3a093fa93d
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-03-23 17:51:24 +0100

    simplified previous tests, added test with different length but with a common part

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -14,74 +14,78 @@ class SimilarityFinderTest {
         int[] array2 = {};
         SequenceSearcher sequenceSearcher = (elem, sequence) -> SearchResult.builder().build();
         SimilarityFinder finder = new SimilarityFinder(sequenceSearcher);
-        assertEquals(1,finder.calculateJackardSimilarity(array, array2));
+        assertEquals(1, finder.calculateJackardSimilarity(array, array2));
     }
 
     @Test
-    void testEqualsArrays(){
+    void testEqualsArrays() {
         int[] array = {768, 1232, 9123, 50};
         int[] array2 = {768, 1232, 9123, 50};
         SequenceSearcher sequenceSearcher = new SequenceSearcher() {
             @Override
             public SearchResult search(int elem, int[] sequence) {
-                SearchResult searchResult = null;
-                if(elem == 768)
-                    searchResult = SearchResult.builder().withPosition(0).withFound(true).build();
-                else if(elem == 1232)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(true).build();
-                else if(elem == 9123)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(true).build();
-                else if(elem == 50)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(true).build();
-
-                return searchResult;
+                return  SearchResult.builder().withPosition(0).withFound(true).build();
             }
         };
         SimilarityFinder finder = new SimilarityFinder(sequenceSearcher);
-        assertEquals(1,finder.calculateJackardSimilarity(array, array2));
+        assertEquals(1, finder.calculateJackardSimilarity(array, array2));
     }
 
 
     @Test
-    void testCompletelyDifferentArrays(){
+    void testCompletelyDifferentArrays() {
         int[] array = {0, 2354, 100, 700};
-        int[] array2 = {678, 2, 9120, 32};
+        int[] array2 = {678, 2, 9120, 32, 600};
         SequenceSearcher sequenceSearcher = new SequenceSearcher() {
             @Override
             public SearchResult search(int elem, int[] sequence) {
-                SearchResult searchResult = null;
-                if(elem == 0)
-                    searchResult = SearchResult.builder().withPosition(0).withFound(false).build();
-                else if(elem == 2354)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(false).build();
-                else if(elem == 100)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(false).build();
-                else if(elem == 700)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(false).build();
-                return searchResult;
+                return SearchResult.builder().withPosition(0).withFound(false).build();
             }
         };
         SimilarityFinder finder = new SimilarityFinder(sequenceSearcher);
-        assertEquals(0,finder.calculateJackardSimilarity(array, array2));
+        assertEquals(0, finder.calculateJackardSimilarity(array, array2));
     }
 
     @Test
-    void testDifferentArrays(){
+    void testDifferentArrays() {
         int[] array = {34, 55};
         int[] array2 = {5, 34};
         SequenceSearcher sequenceSearcher = new SequenceSearcher() {
             @Override
             public SearchResult search(int elem, int[] sequence) {
                 SearchResult searchResult = null;
-                if(elem == 34)
-                    searchResult = SearchResult.builder().withPosition(0).withFound(true).build();
-                else if(elem == 55)
-                    searchResult = SearchResult.builder().withPosition(1).withFound(false).build();
+                boolean[] types = {true, false};
+                for (int i = 0; i < array.length; i++) {
+                    if (elem == array[i]) {
+                        searchResult = SearchResult.builder().withPosition(0).withFound(types[i]).build();
+                    }
+                }
                 return searchResult;
             }
         };
         SimilarityFinder finder = new SimilarityFinder(sequenceSearcher);
-        assertEquals(1.0/3.0,finder.calculateJackardSimilarity(array, array2));
+        assertEquals(1.0 / 3.0, finder.calculateJackardSimilarity(array, array2));
+    }
+
+    @Test
+    void testArraysWithDifferentLengthButWithCommonPart() {
+        int[] array = {34, 55, 456, 654, 66, 4};
+        int[] array2 = {34, 55, 123};
+        SequenceSearcher sequenceSearcher = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                SearchResult searchResult = null;
+                boolean[] types = {true, true, false, false, false, false};
+                for (int i = 0; i < array.length; i++) {
+                    if (elem == array[i]) {
+                        searchResult = SearchResult.builder().withPosition(0).withFound(types[i]).build();
+                    }
+                }
+                return searchResult;
+            }
+        };
+        SimilarityFinder finder = new SimilarityFinder(sequenceSearcher);
+        assertEquals(2.0 / 7.0, finder.calculateJackardSimilarity(array, array2));
     }
 
 }
